65b63bc16057740eff5a4fcf3be673e95d90d13f
Guy Daher <guy.daher@algolia.com>
2017-03-23 14:54:08 +0100

    Demo complete

--- BasicDemo/BasicDemo/FacetController.swift
+++ BasicDemo/BasicDemo/FacetController.swift
@@ -3,24 +3,24 @@ import UIKit
 import InstantSearchCore
 
 class FacetController: UIViewController
-//                    , FacetDataSource
+                    , FacetDataSource
 {
     
     var instantSearchBinder: InstantSearchBinder!
     @IBOutlet weak var refinementList: RefinementListWidget!
     
-//    override func viewDidLoad() {
-//        refinementList.facetDataSource = self
-//        instantSearchBinder.add(widget: refinementList)
-//    }
-//    
-//    func cellFor(facetValue: FacetValue, isRefined: Bool, at indexPath: IndexPath) -> UITableViewCell {
-//        let cell = refinementList.dequeueReusableCell(withIdentifier: "facetCell", for: indexPath)
-//        
-//        cell.textLabel?.text = facetValue.value
-//        cell.detailTextLabel?.text = String(facetValue.count)
-//        cell.accessoryType = isRefined ? .checkmark : .none
-//        
-//        return cell
-//    }
+    override func viewDidLoad() {
+        refinementList.facetDataSource = self
+        instantSearchBinder.add(widget: refinementList)
+    }
+    
+    func cellFor(facetValue: FacetValue, isRefined: Bool, at indexPath: IndexPath) -> UITableViewCell {
+        let cell = refinementList.dequeueReusableCell(withIdentifier: "facetCell", for: indexPath)
+        
+        cell.textLabel?.text = facetValue.value
+        cell.detailTextLabel?.text = String(facetValue.count)
+        cell.accessoryType = isRefined ? .checkmark : .none
+        
+        return cell
+    }
 }
