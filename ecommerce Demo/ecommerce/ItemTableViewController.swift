11fe98162a30e11707508696d326021ea586c91d
Vladislav Fitc <vladislav.fitz@gmail.com>
2019-07-19 13:43:10 +0200

    Changed search view the way its done by Metrodeal

--- ecommerce Demo/ecommerce/ItemTableViewController.swift
+++ ecommerce Demo/ecommerce/ItemTableViewController.swift
@@ -18,8 +18,10 @@ class ItemTableViewController: HitsTableViewController {
     @IBOutlet weak var nbHitsLabel: UILabel!
     
 //    var hitsController: HitsController!
-    var searchController: UISearchController!
-    
+  
+    private var searchBarWidget: SearchBarWidget?
+//    var searchController: UISearchController!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         self.definesPresentationContext = true
@@ -35,6 +37,11 @@ class ItemTableViewController: HitsTableViewController {
 //        tableView.delegate = hitsController
 //        hitsController.tableDataSource = self
     }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    searchBarView.becomeFirstResponder()
+  }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath, containing hit: [String : Any]) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "recordCell", for: indexPath) as! ItemCell
@@ -48,7 +55,8 @@ class ItemTableViewController: HitsTableViewController {
     // MARK: Helper methods for configuring each component of the table
     
     func configureInstantSearch() {
-        InstantSearch.shared.register(searchController: searchController)
+//        InstantSearch.shared.register(searchController: searchController)
+        InstantSearch.shared.register(widget: searchBarWidget!)
         InstantSearch.shared.registerAllWidgets(in: self.view)
     }
     
@@ -69,18 +77,37 @@ class ItemTableViewController: HitsTableViewController {
     
     func configureSearchController() {
         // Initialize and perform a minimum configuration to the search controller.
-        searchController = UISearchController(searchResultsController: nil)
-        
-        searchController.dimsBackgroundDuringPresentation = false
-        searchController.hidesNavigationBarDuringPresentation = false
-        
-        searchController.searchBar.placeholder = "Search items"
-        searchController.searchBar.sizeToFit()
-        
-        searchController.searchBar.barTintColor = ColorConstants.barBackgroundColor
-        searchController.searchBar.isTranslucent = false
-        searchController.searchBar.layer.cornerRadius = 1.0
-        searchController.searchBar.clipsToBounds = true
-        searchBarView.addSubview(searchController.searchBar)
+//        searchController = UISearchController(searchResultsController: nil)
+//
+//        searchController.dimsBackgroundDuringPresentation = false
+//        searchController.hidesNavigationBarDuringPresentation = false
+//
+//        searchController.searchBar.placeholder = "Search items"
+//        searchController.searchBar.sizeToFit()
+//
+//        searchController.searchBar.barTintColor = ColorConstants.barBackgroundColor
+//        searchController.searchBar.isTranslucent = false
+//        searchController.searchBar.layer.cornerRadius = 1.0
+//        searchController.searchBar.clipsToBounds = true
+//        searchBarView.addSubview(searchBarWidget!)
+      searchBarWidget = SearchBarWidget(frame: searchBarView.frame)
+      searchBarWidget?.placeholder = "Search items"
+      searchBarWidget?.sizeToFit()
+      
+      searchBarWidget?.barTintColor = UIColor.clear
+      searchBarWidget?.isTranslucent = true
+      searchBarWidget?.layer.cornerRadius = 1.0
+      searchBarWidget?.clipsToBounds = true
+      searchBarWidget?.searchBarStyle = .minimal
+      searchBarWidget?.tintColor = UIColor(red:1.00, green:0.64, blue:0.19, alpha:1.0)
+      
+//      searchBarWidget?.setMagnifyingGlassColorTo(color: .white)
+//      searchBarWidget?.setPlaceholderTextColorTo(color: .white)
+//      searchBarWidget?.setCancelColor(color: .white)
+      searchBarView.addSubview(searchBarWidget!)
+      searchBarView.bringSubviewToFront(searchBarWidget!)
+//      topBarView.backgroundColor = .red
+//      searchBarView.backgroundColor = .green
+      searchBarWidget!.frame = .init(x: 0, y: 0, width: searchBarView.frame.width, height: searchBarView.frame.height) //.init(x: -100, y: 0, width: 320, height: 44)
     }
 }
